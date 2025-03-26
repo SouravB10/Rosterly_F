345a9b434569e4343c0921fe506295923609ec49
unknown <naveennagam6015@gmail.com>
2025-03-26 13:07:00 +0530

    Routs and login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,28 @@ import './App.css'
 import { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import Login from './Screens/Login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   useEffect(() => {
     AOS.init({
-      duration: 1500, 
-      once: true, 
+      duration: 1500,
+      once: true,
     });
   }, []);
 
   return (
     <>
-      <h1 className='bg-purple-500 sub-heading font-bold text-white text-center p-4'>Rosterly</h1>
+      {/* <h1 className='bg-purple-500 sub-heading font-bold text-white text-center p-4'>Rosterly</h1>
       <div className=' flex justify-center mt-4'>
         <button className='button' data-aos="flip-left">Submit</button>
-      </div>
+      </div> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
