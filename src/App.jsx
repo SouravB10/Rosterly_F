f3d11aa092000a5eca7e1122bf327d9d2b2dcf29
SouravB10 <souravbehuria10@gmail.com>
2025-03-28 17:51:17 +0530

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import 'aos/dist/aos.css';
 import Login from './Screens/Login';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from './Component/SideBar';
+import Rosterly from './Screens/Rosterly';
+import Unavailability from './Screens/Unavailability';
 
 function App() {
 
@@ -16,6 +18,8 @@ function App() {
           <div className='flex-1'>
             <Routes>
               <Route path='/login' element={<Login />} />
+              <Route path='/myrosterly' element={<Rosterly/>} />
+              <Route path='/unavailability' element={<Unavailability />} />
             </Routes>
           </div>
         </div>
