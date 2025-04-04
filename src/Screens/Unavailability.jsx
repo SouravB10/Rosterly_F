06bab675aec7dafb321215a4794111c2f46f8bf7
Glansa-Solutions <glansa.solutions777@gmail.com>
2025-04-04 18:36:21 +0530

    Unavailability

--- src/Screens/Unavailability.jsx
+++ src/Screens/Unavailability.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Unavailability = () => {
   return (
     <>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, consequatur sequi. Veniam doloribus perspiciatis dignissimos quo laudantium laborum, ex ipsum? Consequatur eum accusamus nesciunt mollitia praesentium totam, minima ipsum exercitationem necessitatibus molestias facere officia vitae veniam nulla, aliquid reiciendis at quam, voluptatibus autem modi pariatur nobis adipisci molestiae! Similique, culpa?</p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 h-full">
+        {/* Days Off Adding */}
+        <div className="bg-gray-100 rounded p-3 md:row-span-5">
+          Days Off Adding
+        </div>
+        <div className="bg-gray-100 rounded p-3 md:col-span-1 md:row-span-13">
+          Recurring Unavailability
+        </div>
+        {/* Requested Days Off List */}
+        <div className="bg-gray-100 rounded p-3 md:row-span-8">
+          Requested Days Off List
+        </div>
+
+        {/* Recurring Unavailability (Full Height in Large Screens) */}
+      </div>
     </>
   );
-}
+};
 
-export default Unavailability
+export default Unavailability;
