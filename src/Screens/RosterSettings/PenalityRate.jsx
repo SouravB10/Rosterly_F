853e3cb0b877b77c5be276c71959b886f229a97e
AnitaSeth1 <anita.glansa@gmail.com>
2025-04-09 17:48:59 +0530

    notification

--- src/Screens/RosterSettings/PenalityRate.jsx
+++ src/Screens/RosterSettings/PenalityRate.jsx
@@ -11,10 +11,10 @@ const PenalityRate = () => {
   ];
 
   return (
-    <div className="p-8">
+    <div className="card p-8">
       {/* Top header bar */}
       <div className="flex items-center justify-between bg-gray-200 rounded-t-lg px-4 py-3">
-        <p className="text-sm text-gray-700">
+        <p className="paragraphThin">
           Every Shift Can Be Checked Against Rule To Determine If Any  Penality Rate Applies.
         </p>
         <div className="flex gap-2">
@@ -31,42 +31,42 @@ const PenalityRate = () => {
       <table className="w-full border border-gray-300">
             <thead>
                 <tr className="bg-[#2f2378] text-white">
-                <th className="border border-gray-300 px-4 py-2 text-left">Priorirt</th>
-                <th className="border border-gray-300 px-4 py-2 text-left">Name</th>
-                <th className="border border-gray-300 px-4 py-2 text-left">Short Name</th>
-                <th className="border border-gray-300 px-4 py-2 text-left">Factor</th>
-                <th className="border border-gray-300 px-4 py-2 text-left">Day</th>
-                <th className="border border-gray-300 px-4 py-2 text-left">Applicable Time</th>
-                <th className="border border-gray-300 px-4 py-2 text-left">Employee Types</th>
+                <th className="subHeading border border-gray-300 px-4 py-2 text-left">Priorirt</th>
+                <th className="subHeading border border-gray-300 px-4 py-2 text-left">Name</th>
+                <th className="subHeading border border-gray-300 px-4 py-2 text-left">Short Name</th>
+                <th className="subHeading border border-gray-300 px-4 py-2 text-left">Factor</th>
+                <th className="subHeading border border-gray-300 px-4 py-2 text-left">Day</th>
+                <th className="subHeading border border-gray-300 px-4 py-2 text-left">Applicable Time</th>
+                <th className="subHeading border border-gray-300 px-4 py-2 text-left">Employee Types</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                <td className="border border-gray-300 px-4 py-2">1</td>
-                <td className="border border-gray-300 px-4 py-2">Public Holiday Hours</td>
-                <td className="border border-gray-300 px-4 py-2">Public Hol</td>
-                <td className="border border-gray-300 px-4 py-2">2.75 X Base</td>
-                <td className="border border-gray-300 px-4 py-2">Public Holiday</td>
-                <td className="border border-gray-300 px-4 py-2">All Day</td>
-                <td className="border border-gray-300 px-4 py-2">FT PT CAS</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">1</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Public Holiday Hours</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Public Hol</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">2.75 X Base</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Public Holiday</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">All Day</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">FT PT CAS</td>
                 </tr>
                 <tr>
-                <td className="border border-gray-300 px-4 py-2">2</td>
-                <td className="border border-gray-300 px-4 py-2">Sunday Hours</td>
-                <td className="border border-gray-300 px-4 py-2">Sun Hol</td>
-                <td className="border border-gray-300 px-4 py-2">2.75 X Base</td>
-                <td className="border border-gray-300 px-4 py-2">Sunday</td>
-                <td className="border border-gray-300 px-4 py-2">All Day</td>
-                <td className="border border-gray-300 px-4 py-2">FT PT CAS</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">2</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Sunday Hours</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Sun Hol</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">2.75 X Base</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Sunday</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">All Day</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">FT PT CAS</td>
                 </tr>
                 <tr>
-                <td className="border border-gray-300 px-4 py-2">3</td>
-                <td className="border border-gray-300 px-4 py-2">Saturday Hours</td>
-                <td className="border border-gray-300 px-4 py-2">Sat Hol</td>
-                <td className="border border-gray-300 px-4 py-2">2.75 X Base</td>
-                <td className="border border-gray-300 px-4 py-2">Saturday</td>
-                <td className="border border-gray-300 px-4 py-2">All Day</td>
-                <td className="border border-gray-300 px-4 py-2">FT PT CAS</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">3</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Saturday Hours</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Sat Hol</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">2.75 X Base</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">Saturday</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">All Day</td>
+                <td className="paragraphThin border border-gray-300 px-4 py-2">FT PT CAS</td>
                 </tr>
                 
                 {/* Add the rest of your rows similarly */}
