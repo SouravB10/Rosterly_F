a16e745b8969724d719030477571dc08c67d04a5
Glansa-Solutions <glansa.solutions777@gmail.com>
2025-05-28 11:51:53 +0530

    git add

--- src/Screens/Rosters.jsx
+++ src/Screens/Rosters.jsx
@@ -513,7 +513,7 @@ const Rosters = () => {
                 <tr key={emp.id} className="border border-gray-300">
                   {/* Employee Info */}
                   <td className="p-2 bg-white">
-                    <div className="font-semibold">{emp.firstName}{" "}{emp.lastName}</div>
+                    <div className="font-semibold">{emp.fullName}</div>
                     <div className="text-xs text-gray-500">
                       {/* {emp.hours} hrs {emp.cost ? `· ${emp.cost}` : ""} */}
                     </div>
